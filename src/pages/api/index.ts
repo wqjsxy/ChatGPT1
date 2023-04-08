9a62ffa4bada89e8c3bfe0bbeea9e1ddd8d9f928
wqjsxy <126588006+wqjsxy@users.noreply.github.com>
2023-04-08 20:08:35 +0800

    Update index.ts

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -114,7 +114,7 @@ export const post: APIRoute = async context => {
           "Content-Type": "application/json",
           Authorization: `Bearer ${apiKey}`
         },
-        timeout: 10000,
+        timeout: 25000,
         method: "POST",
         body: JSON.stringify({
           model: "gpt-3.5-turbo",
